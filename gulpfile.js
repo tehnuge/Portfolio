c526f9f032552f4a82cd006c7049969abf4f86ca
John Duong <jhnduong@gmail.com>
2016-05-02 13:08:52 -0700

    added browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,24 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+var browserSync = require('browser-sync').create();
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./sass/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());;
 });
  
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-});
+ gulp.task('default', ['serve']);
